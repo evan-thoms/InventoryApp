ca4c3099e8190330cddd482cb5380cae542e2db0
evan-thoms <evanthoms@outlook.com>
2024-08-01 23:50:25 -0700

    initial commit

---
+++ firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZzKmRsphOdTQTRJNxf9jJ47golHr6xsw",
+  authDomain: "hspantry-f443d.firebaseapp.com",
+  projectId: "hspantry-f443d",
+  storageBucket: "hspantry-f443d.appspot.com",
+  messagingSenderId: "682698282593",
+  appId: "1:682698282593:web:ca038954c76d990f49fc20",
+  measurementId: "G-YEKQFPPB6P"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export {firestore}
